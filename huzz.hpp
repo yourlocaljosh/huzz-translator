2c7c78ae055b1f5b265dfdf6b3de390105b397b6
Josh Hong <joshsw@umich.edu>
2024-12-14 04:19:22 -0500

    Added overloaded function for translate

--- huzz.hpp
+++ huzz.hpp
@@ -20,7 +20,9 @@ void translate() {
     std::string result;
 
     while (stream >> word) {
-        if (word == "hoes") {
+        std::string lower_word = word;
+        std::transform(lower_word.begin(), lower_word.end(), lower_word.begin(), ::tolower);
+        if (lower_word == "hoes") {
             word = "huzz";
         } else if (word.length() > 1 && is_vowel(word[word.length() - 2])) {
             word.pop_back();
@@ -37,6 +39,33 @@ void translate() {
     }
 
     std::cout << result << std::endl;
+}
+
+std::string translate(const std::string& sentence){
+    std::istringstream stream(sentence);
+    std::string word;
+    std::string result;
+
+    while (stream >> word) {
+        std::string lower_word = word;
+        std::transform(lower_word.begin(), lower_word.end(), lower_word.begin(), ::tolower);
+        if (lower_word == "hoes") {
+            word = "huzz";
+        } else if (word.length() > 1 && is_vowel(word[word.length() - 2])) {
+            word.pop_back();
+            word += "uzz";
+        } else if (!word.empty() && is_vowel(word.back())) {
+            word.pop_back();
+            word += "uzz"; 
+        }
+        result += word + " ";
+    }
+
+    if (!result.empty()) {
+        result.pop_back();
+    }
+
+    return result;
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include "huzz.hpp"
+#include <iostream>
+
+using namespace std;
+
+//some default test cases
 
 int main() {
+    //Hoes is a word that refers to farming tools, but in this case they
+    //refer to farming tools from MINECRAFT
+    cout << translate("Hey Hoes") << endl;
+    cout << translate("Hey, how are the hoes doing? Have they been sharpened in a while?") << endl;
+    cout << translate("Me when bro scaring the hoes away by mentioning how we spent 24 hours on roblox last night") << endl;
     translate();
     return 0;
 }
